dc406011edc7a5fcbdeea3f6a06a0181605ab856
nordahn87 <charleynordahn87@gmail.com>
2023-11-16 20:47:23 +0100

    I did stuff
    I did stuff and it works somehow

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,9 @@
+export const Buttons = () => {
+    return (
+        <div className="buttons">
+            <button className="button">Edit</button>
+            <button className="button">Create</button>
+            <button className="button">Delete</button>
+        </div>
+    );
+};
